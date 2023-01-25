c013e8ad3e8ae0b212a93c25af8eca4945b323a1
davidbreau <davidbreau@live.fr>
2023-01-25 14:39:14 +0100

    Thons desormais imputables dans la mer

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
 import random
-from utils import Grille
+from utils import Grille, Thon
 thon = "🐟"
 requin = "🦈"
 vide = "░░"
 
 
-            
 Grille.position[random.randint(0,9)][random.randint(0,9)] = requin
 Grille.position[random.randint(0,9)][random.randint(0,9)] = thon
 
-Grille.afficher()
 
+tuna1 = Thon(1,2)
+tuna2 = Thon(3,4)
+tuna3 = Thon(4,5)
+liste_de_thon = [tuna1, tuna2, tuna3]
+
+for thon in liste_de_thon:
+    Grille.position[thon.x][thon.y] = Thon.emote
+    
+Grille.afficher()
--- utils.py
+++ utils.py
@@ -4,6 +4,7 @@ class Grille:
         position.append(["░░"])
         for colonne in range(10):
             position[rangee].append("░░")
+    
 
     def afficher():
         affichage = ""
@@ -12,3 +13,24 @@ class Grille:
             for j in range(10):
                 affichage += Grille.position[i][j]
         print(affichage)
+        
+    def set(x: int, y: int):
+        return Grille.position[x][y]
+    
+
+        
+class Thon:
+    emote = "🐟"
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.reproduction = 10
+        
+    
+
+
+    
+
+
+    
+    
