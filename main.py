7bb4f4add8c2b2c819a0c694b7a223f67a5341d4
davidbreau <davidbreau@live.fr>
2023-01-25 11:36:54 +0100

    Travail sur l'affichage de la grille

--- main.py
+++ main.py
@@ -1,2 +1,29 @@
-poisson = "🐟"
+import random
+
+thon = "🐟"
 requin = "🦈"
+vide = "  "
+
+grille = []
+for rangee in range(10):
+    grille.append(["░░"])
+    for colonne in range(10):
+        grille[rangee].append("░░")
+
+def afficher_grille():
+        affiche_grille = ""   
+        for i in range(10):
+            affiche_grille += "\n"
+            for j in range(10):
+                affiche_grille += grille[i][j]
+        print(affiche_grille)
+
+grille[random.randint(0,9)][random.randint(0,9)] = requin
+
+grille[1][9] = thon
+
+
+afficher_grille()
+
+
+
