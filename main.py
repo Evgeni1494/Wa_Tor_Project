eb1a9523232a2e106c35b58a3f15a3310b0c7297
davidbreau <davidbreau@live.fr>
2023-01-30 21:37:05 +0100

    Chiffres initiaux creaient desormais un équilibre

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 from jeu import Jeu
 
+#parametres initiaux 
 dictionnaire_de_param = {
-    "x_len" : 20, #hauteur de la grille
-    "y_len" : 20,
-    "nb_requins" : 10,
-    "nb_poissons" : 80,
-    "nb_de_tour_pour_reproduction_requin" : 9,
-    "nb_de_tour_pour_reproduction_poisson" : 2,
-    "energy_par_poisson" : 3,
-    "start_energy" :  4
+    "x_len" : 30, #hauteur de la grille
+    "y_len" : 50, #largeur de la grille
+    "nb_requins" : 10, 
+    "nb_poissons" : 200, 
+    "nb_de_tour_pour_reproduction_requin" : 15, 
+    "nb_de_tour_pour_reproduction_poisson" : 10,
+    "energy_par_poisson" : 2, 
+    "start_energy" :  5
 }
 
 mon_jeu =Jeu(**dictionnaire_de_param)
